6e1c88583143340504ef6aca7c104c1733f89d63
david nelband <torun.wikstrom@gmail.com>
2022-12-26 18:10:47 +0100

    added vars to next run time config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,22 @@
 import { initializeApp } from 'firebase/app';
+import getConfig from 'next/config';
+
+const {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} = getConfig().publicRuntimeConfig;
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
-
-console.log(firebaseConfig);
 export const app = initializeApp(firebaseConfig);
