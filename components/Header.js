efef48b1727c7888db9e80ef64cfb8f2c8968e4c
TorunW <torun.wikstrom@gmail.com>
2024-04-30 19:11:09 +0200

    update header ui

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,16 @@ import headerStyles from '../styles/Header.module.css';
 const Header = () => {
   return (
     <header className={headerStyles.header}>
-      <div className={headerStyles.bg}>
-        <div className={headerStyles.text}>
-          <span className={headerStyles.border}>Charlotte Hillborg</span>
-        </div>
+      <div className={headerStyles.content}>
+        <span className={headerStyles.circleOneOuter}>
+          <span className={headerStyles.circleOneInner}></span>
+        </span>
+        <span className={headerStyles.circleTwoOuter}>
+          <span className={headerStyles.circleTwoInner}></span>
+        </span>
+        <span className={headerStyles.circleThreeOuter}>
+          <span className={headerStyles.circleThreeInner}></span>
+        </span>
       </div>
     </header>
   );
